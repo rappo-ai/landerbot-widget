176ebc1d59b6bb1fab85b0ae2f121adbee545fd4
Renzil DSouza <renzil@gmail.com>
2021-08-18 03:25:43 +0530

    Updating the rasa server base url

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "http://localhost:5006/webhooks/rest";
+const rasa_server_url = "https://client.landerbot.rappo.ai/webhooks/rest";
 const sender_id = uuidv4();
