8761f5fabeaa6b36bc4ceac551503179d678852c
lsabbani <narayana.sabbani@gmail.com>
2021-08-25 09:00:53 +0530

    Chatbot script Integration and dynamic iframe styles
    Chatbot script Integration and dynamic iframe styles

---
+++ static/js/embed.js
@@ -0,0 +1,15 @@
+const Rappo = {}
+window.addEventListener('DOMContentLoaded', (event) => {
+  const chatbotWidget = document.getElementById('chatbotWidget')
+  Rappo.widget = chatbotWidget
+});
+window.Rappo = Rappo
+
+window.addEventListener('message', function (e) {
+  const data = e.data;
+  if (data === 'open') {
+    document.getElementById('chatbotWidget').className = 'chatbot chatbotOpen'
+  } else if (data === 'close') {
+    document.getElementById('chatbotWidget').className = 'chatbot chatbotClose'
+  }
+});
