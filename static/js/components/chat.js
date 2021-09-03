3f59525b9385b0e850fdc0ac0c5449c41af241ae
Renzil DSouza <renzil@gmail.com>
2021-09-04 04:02:57 +0530

    Sending user analytics to admin live chat on /start

--- static/js/components/chat.js
+++ static/js/components/chat.js
@@ -1,4 +1,5 @@
 const rasa_server_url = "https://client-test.landerbot.rappo.ai/webhooks/rest";
+// const rasa_server_url = "http://localhost:5016/webhooks/rest";
 const RappoSenderId = localStorage.getItem('RappoSenderId') || uuidv4();
 localStorage.setItem('RappoSenderId', RappoSenderId)
 
@@ -239,16 +240,19 @@ function setBotResponse(response) {
  * sends the user message to the rasa server,
  * @param {String} message user message
  */
-function send(inputText, message) {
+function send(inputText, message, metadata) {
     if (!message) {
         message = inputText
+    }
+    if (!metadata) {
+        metadata = {}
     }
     console.log('message', message)
     $.ajax({
         url: rasa_server_url + "/webhook",
         type: "POST",
         contentType: "application/json",
-        data: JSON.stringify({ text: message, input_text: inputText, sender: RappoSenderId }),
+        data: JSON.stringify({ text: message, input_text: inputText, metadata: metadata, sender: RappoSenderId }),
         success(botResponse, status) {
             console.log("Response from Rasa: ", botResponse, "\nStatus: ", status);
 
@@ -443,4 +447,87 @@ evtSource.onmessage = function(event) {
     setBotResponse(JSON.parse(event.data));
 }
 
-send("", "/start");
+// https://stackoverflow.com/a/11219680/8471647
+function get_browser_data() {
+    var nVer = navigator.appVersion;
+    var nAgt = navigator.userAgent;
+    var browserName  = navigator.appName;
+    var fullVersion  = ''+parseFloat(navigator.appVersion); 
+    var majorVersion = parseInt(navigator.appVersion,10);
+    var nameOffset,verOffset,ix;
+
+    // In Opera, the true version is after "Opera" or after "Version"
+    if ((verOffset=nAgt.indexOf("Opera"))!=-1) {
+    browserName = "Opera";
+    fullVersion = nAgt.substring(verOffset+6);
+    if ((verOffset=nAgt.indexOf("Version"))!=-1) 
+    fullVersion = nAgt.substring(verOffset+8);
+    }
+    // In MSIE, the true version is after "MSIE" in userAgent
+    else if ((verOffset=nAgt.indexOf("MSIE"))!=-1) {
+    browserName = "Microsoft Internet Explorer";
+    fullVersion = nAgt.substring(verOffset+5);
+    }
+    // In Chrome, the true version is after "Chrome" 
+    else if ((verOffset=nAgt.indexOf("Chrome"))!=-1) {
+    browserName = "Chrome";
+    fullVersion = nAgt.substring(verOffset+7);
+    }
+    // In Safari, the true version is after "Safari" or after "Version" 
+    else if ((verOffset=nAgt.indexOf("Safari"))!=-1) {
+    browserName = "Safari";
+    fullVersion = nAgt.substring(verOffset+7);
+    if ((verOffset=nAgt.indexOf("Version"))!=-1) 
+    fullVersion = nAgt.substring(verOffset+8);
+    }
+    // In Firefox, the true version is after "Firefox" 
+    else if ((verOffset=nAgt.indexOf("Firefox"))!=-1) {
+    browserName = "Firefox";
+    fullVersion = nAgt.substring(verOffset+8);
+    }
+    // In most other browsers, "name/version" is at the end of userAgent 
+    else if ( (nameOffset=nAgt.lastIndexOf(' ')+1) < 
+            (verOffset=nAgt.lastIndexOf('/')) ) 
+    {
+    browserName = nAgt.substring(nameOffset,verOffset);
+    fullVersion = nAgt.substring(verOffset+1);
+    if (browserName.toLowerCase()==browserName.toUpperCase()) {
+    browserName = navigator.appName;
+    }
+    }
+    // trim the fullVersion string at semicolon/space if present
+    if ((ix=fullVersion.indexOf(";"))!=-1)
+    fullVersion=fullVersion.substring(0,ix);
+    if ((ix=fullVersion.indexOf(" "))!=-1)
+    fullVersion=fullVersion.substring(0,ix);
+
+    majorVersion = parseInt(''+fullVersion,10);
+    if (isNaN(majorVersion)) {
+    fullVersion  = ''+parseFloat(navigator.appVersion); 
+    majorVersion = parseInt(navigator.appVersion,10);
+    }
+
+    return {
+        userAgent: navigator.userAgent,
+        appName: navigator.appName,
+        appVersion: navigator.appVersion,
+        browserName: browserName,
+        fullVersion: fullVersion,
+        majorVersion: majorVersion,
+    }
+}
+
+let location_data = {}
+$.ajax('http://ip-api.com/json')
+    .then(
+        function success(response) {
+            location_data = response
+        },
+        function fail(data, status) {
+        }
+    ).then(function done() {
+        send("", "/start", {
+            location_data: location_data,
+            browser_data: get_browser_data(),
+        });
+    });
